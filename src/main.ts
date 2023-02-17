c9da88ce87e5c8e66e732fac67173bcfa8dd89c2
bamboo <815070110@qq.com>
2023-02-17 17:00:57 +0800

    first aa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from "./App.vue";
 //创建pinia实例
 const pinia = createPinia()
-
+//这是一个关于bamboo的测试
 export function createApp() {
   const app = createSSRApp(App);
   app.use(pinia)
