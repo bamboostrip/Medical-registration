957cd4a8f19fd1de40c3b715b610f1fead5ba519
bamboo <815070110@qq.com>
2023-03-31 08:46:20 +0800

    20230331

--- .history/src/main_20230331084550.ts
+++ .history/src/main_20230331084550.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: bamboo
  * @Date: 2023-01-29 16:10:12
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023-02-23 12:47:28
+ * @LastEditors: bamboo 815070110@qq.com
+ * @LastEditTime: 2023-03-31 08:45:50
  * @FilePath: \wx-zhyl\src\main.ts
  * @Description: 
  */
@@ -13,7 +13,7 @@ import App from "./App.vue";
 const pinia = createPinia()
 
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App); //
   app.use(pinia)
   return {
     app,
--- .history/src/main_20230331084552.ts
+++ .history/src/main_20230331084552.ts
@@ -2,7 +2,7 @@
  * @Author: bamboo
  * @Date: 2023-01-29 16:10:12
  * @LastEditors: bamboo 815070110@qq.com
- * @LastEditTime: 2023-02-17 17:11:05
+ * @LastEditTime: 2023-03-31 08:45:50
  * @FilePath: \wx-zhyl\src\main.ts
  * @Description: 
  */
@@ -11,10 +11,9 @@ import { createPinia } from 'pinia'
 import App from "./App.vue";
 //创建pinia实例
 const pinia = createPinia()
-//测试
 
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App); // 创建vue实例
   app.use(pinia)
   return {
     app,
